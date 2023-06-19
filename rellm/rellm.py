c14f6f8dfe66bd6f7c714ce1c95d808e18db128d
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-19 08:16:06 +0000

    * Managing what devices tensors are stored on during inference * Added Different stopping condition

--- rellm/rellm.py
+++ rellm/rellm.py
@@ -8,8 +8,8 @@ from rellm.logits_mask import LogitsMask
 from rellm.re_token_filter import ReTokenFilter
 
 
-def complete_re(prompt:str, pattern: regex.Pattern | List[regex.Pattern], tokenizer: PreTrainedTokenizer, 
-                model: PreTrainedModel, max_new_tokens: int = 3, 
+def complete_re(prompt:str, pattern: regex.Pattern | List[regex.Pattern], tokenizer: PreTrainedTokenizer,
+                model: PreTrainedModel, max_new_tokens: int = 3,
                 stop_after_match: bool = True,
                 debug: bool = False,
                 **model_kwargs):
@@ -18,7 +18,10 @@ def complete_re(prompt:str, pattern: regex.Pattern | List[regex.Pattern], tokeni
     """
     if isinstance(pattern, regex.Pattern):
         pattern = [pattern]
-        
+
+    if len(pattern) == 1 and is_constant_regex(pattern[0].pattern):
+        return pattern[0].pattern
+
     gen_tokens = 0
     partial_completion = ""
     prompt_plus_completion = prompt + partial_completion
@@ -32,14 +35,15 @@ def complete_re(prompt:str, pattern: regex.Pattern | List[regex.Pattern], tokeni
         allowed_token_ids = token_filter.filter_tokens(partial_completion, pattern)
         custom_mask_processor = LogitsMask(allowed_token_ids)
 
-        output_ids = model.generate(prompt_token_ids,
+        output_ids = model.generate(prompt_token_ids.to(model.device),
                                     max_new_tokens=1,
                                     pad_token_id=tokenizer.eos_token_id,
                                     logits_processor=[custom_mask_processor],
                                     **model_kwargs
         )
-        new_token_ids = output_ids[0, prompt_length:]
+        new_token_ids = output_ids[0, prompt_length:].to("cpu")
         output_text = tokenizer.decode(new_token_ids, skip_special_tokens=True)
+        previous_partial_completion = partial_completion
         partial_completion += output_text
         prompt_plus_completion = prompt_plus_completion + output_text
         if debug:
@@ -48,8 +52,36 @@ def complete_re(prompt:str, pattern: regex.Pattern | List[regex.Pattern], tokeni
         if stop_after_match:
             for p in pattern:
                 m = p.match(partial_completion)
-                if m and m.start() == 0:
-                    return m[0]
+                if m:
+                    if m.start() == 0 and m.end() < (len(partial_completion) - 5):
+                        return m[0]
+                    if previous_partial_completion == partial_completion:
+                        return m[0]
+
         gen_tokens += 1
 
     return partial_completion
+
+
+def is_constant_regex(pattern):
+    # Escaped characters to be considered when checking for a constant regex pattern
+    escaped_chars = r'\.*+?{}()[]|^$'
+
+    # Return False if the pattern is empty
+    if not pattern:
+        return False
+
+    i = 0
+    while i < len(pattern):
+        # Check for unescaped special regex characters
+        char = pattern[i]
+        if char in escaped_chars:
+            if i == 0 or (i > 0 and pattern[i-1] != '\\'):
+                return False
+            else:
+                # Skip one character when an escaped special regex character is preceded by two backslashes
+                if (i > 1) and (pattern[i-2] == '\\'):
+                    i += 1
+        i += 1
+
+    return True
