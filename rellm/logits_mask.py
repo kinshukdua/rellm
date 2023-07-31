c52a25d0eb017c41a22fc6017406f27e26332d67
Kinshuk Dua <kinshukdua@gmail.com>
2023-07-31 20:11:50 +0530

    optimize logits_mask.py with vector operation

--- rellm/logits_mask.py
+++ rellm/logits_mask.py
@@ -9,13 +9,12 @@ class LogitsMask(LogitsProcessor):
     not in the allowed token ids set.
     """
     def __init__(self, allowed_token_ids):
-        self.allowed_token_ids = set(allowed_token_ids)
+        self.allowed_token_ids = list(set(allowed_token_ids))
 
     def __call__(self, input_ids, scores):
         device = scores.device
         scores = scores.cpu()
         mask = np.ones_like(scores) * -1e10
-        for token_id in self.allowed_token_ids:
-            mask[:, token_id] = 0
+        mask[:, self.allowed_token_ids] = 0
         scores = scores + mask 
         return scores.to(device)
